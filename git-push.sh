93b8cd4da5edbe8292e92170cf8e5a3ba2ac510a
xiaoni <xiaoni0601@gmail.com>
2022-02-07 14:08:57 +0800

    close gitee.io

--- git-push.sh
+++ git-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '=========================pushing gitee.com'
-git push cnorigin master
+#echo '=========================pushing gitee.com'
+#git push cnorigin master
 
 echo '=========================pushing github.com'
 git push origin master
